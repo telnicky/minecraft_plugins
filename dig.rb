19e016426ee1982e478092b491ab8826652d842c
Travis Elnicky <telnicky@instructure.com>
2014-12-02 21:02:59 -0700

    more dig

--- dig.rb
+++ dig.rb
@@ -3,18 +3,12 @@ class DigPlugin
 	description 'Dig', 0.1
 
   def on_enable
-    state = {}
     public_command('dig', 'dig down!', '/dig {distance}') do |player, *args|
-      depth = args[0].to_i || 1
-      state[player.name] = depth
-      player.msg("depth has been set to #{ depth }")
-    end
-
-    event(:block_break) do |event|
-      return unless state[event.player.name]
-
-      state[event.player.name].times do
-        event.block.block_at(:down, counter).change_type(:air)
+      block = player.target_block
+      depth = args[0].to_i
+      type  = args[1] || :air
+      depth.times do |counter|
+        block.block_at(:down, counter).change_type(type)
       end
     end
   end
